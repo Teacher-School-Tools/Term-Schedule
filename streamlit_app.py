7e5c4db326135181de4f82c2baa40b1eb28140f6
Ricky Fan <ricky.fan@uwaterloo.ca>
2026-09-15 19:11:00 -0400

    reverting, just ignore the other apps

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 
-pages = [
-    st.Page("pages/0_term_schedule_app.py", title="Scheduler"),
-    st.Page("pages/1_group_randomizer_app.py", title="Randomizer"),
-]
+# pages = [
+#     st.Page("pages/0_term_schedule_app.py", title="Scheduler"),
+#     st.Page("pages/1_group_randomizer_app.py", title="Randomizer"),
+# ]
 
-pg = st.navigation(pages)
+# pg = st.navigation(pages)
 
-st.write("Select one of the apps to use.")
+st.write("Select one of the apps to use on the side bar.")
 
-if st.button("Go to Scheduler"):
-    st.switch_page("pages/0_term_schedule_app.py")
+# if st.button("Go to Scheduler"):
+#     st.switch_page("pages/0_term_schedule_app.py")
 
-if st.button("Go to Randomizer"):
-    st.switch_page("pages/1_group_randomizer_app.py")
+# if st.button("Go to Randomizer"):
+#     st.switch_page("pages/1_group_randomizer_app.py")
