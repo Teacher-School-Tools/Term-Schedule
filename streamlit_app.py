e8b2d06a4a2b561b7ac58fdc19da2e396a5f29ef
Ricky Fan <ricky.fan@uwaterloo.ca>
2026-09-15 16:15:15 -0400

    added buttons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.write("Select one of the apps to use.")
+
+if st.button("Go to Scheduler"):
+    st.switch_page("pages/0_term_schedule_app.py")
+
+if st.button("Go to Randomizer"):
+    st.switch_page("pages/1_group_randomizer_app.py")
