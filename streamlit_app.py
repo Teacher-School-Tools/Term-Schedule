0d820278e28ed24cc1d7ce2421e9d101a5c13a9c
Ricky Fan <ricky.fan@uwaterloo.ca>
2026-09-15 18:57:52 -0400

    updated navigator

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+pages = [
+    st.Page("pages/0_term_schedule_app.py", title="Scheduler"),
+    st.Page("pages/1_group_randomizer.py", title="Randomizer"),
+]
+
+pg = st.navigation(pages)
+
 st.write("Select one of the apps to use.")
 
 if st.button("Go to Scheduler"):
